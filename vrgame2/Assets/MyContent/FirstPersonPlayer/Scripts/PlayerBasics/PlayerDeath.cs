bd8dc365911002c5cb7e7f5059f2bcbc350be11e
Snainner <senne010@gmail.com>
2023-11-04 17:13:25 +0100

    Game over menu is now working with a restart button

--- vrgame2/Assets/MyContent/FirstPersonPlayer/Scripts/PlayerBasics/PlayerDeath.cs
+++ vrgame2/Assets/MyContent/FirstPersonPlayer/Scripts/PlayerBasics/PlayerDeath.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerDeath : MonoBehaviour
 {
     public GameObject DeathUI;
-
+    
     [SerializeField] private int Health = 2;
 
     private void Start()
     {
         DeathUI = GameObject.FindGameObjectWithTag("DeathUI"); 
         DeathUI.SetActive(false);
-        Health = 2; 
+        Health = 2;
+        Time.timeScale = 1;
+        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -21,14 +24,20 @@ public class PlayerDeath : MonoBehaviour
         {
             Health -= 1;
         }
+        if (Health <= 0)
+        {
+            Death();
+        }
     }
 
     private void Death()
     {
-        if (Health <= 0)
-        {
-            DeathUI.SetActive(true);
+        DeathUI.SetActive(true);
+        Time.timeScale = 0;
+    }
 
-        }
+    public void RestartButton()
+    {
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
