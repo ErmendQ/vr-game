73026db097ca10a46836ded627f8db572a7fd536
Snainner <senne010@gmail.com>
2023-11-06 16:09:28 +0100

    Added dialogue system for better subtitles

---
+++ vrgame2/Assets/MyContent/Scripts/Dialogue.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro; 
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] subLines;
+    [SerializeField] float textSpeed;
+    [SerializeField] float nextLineSpeed = 5;
+
+    private int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        StartDialogue();
+        
+        
+    }
+
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+
+        //if (textComponent.text == subLines[index])
+        //{
+        //    //StartCoroutine(WaitForNextLine());
+        //}
+        //else
+        //{
+        //    StopAllCoroutines();
+        //    textComponent.text = subLines[index];
+
+        //}
+    }
+
+    void NextLine()
+    {
+        if (index < subLines.Length - 1)
+        {
+            index++;
+            Debug.Log(index);
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+          
+        }
+        else
+        {
+            gameObject.SetActive(false);
+           
+        }
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach(char c in subLines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return  new  WaitForSeconds(textSpeed);
+        }
+       
+        yield return new WaitForSeconds(nextLineSpeed);
+        NextLine();
+
+    }
+
+    //IEnumerator WaitForNextLine()
+    //{
+    //    for (int i = 0; i < subLines.Length; i++)
+    //    {
+    //        i++;
+    //        index++;
+    //        yield return new WaitForSeconds(nextLineSpeed);
+    //        NextLine();
+    //    }
+    //    yield return new WaitForSeconds(nextLineSpeed);
+    //    NextLine();
+    //}
+}
